9406f64d4f608f1029c16a8bc7ee335b0fc0cbef
Pinna Maxime <max.pinna@hotmail.fr>
2022-04-28 09:55:34 +0200

    Vérification si on est en DEV ou en PROD

--- exo1/index.js
+++ exo1/index.js
@@ -2,3 +2,6 @@ console.log("HELLO WORLD");
 
 const mavariable = process.env["MAVARIABLE"];
 console.log(mavariable);
+
+const contexte = mavariable ? "JE SUIS EN DEV" : "JE SUIS EN PROD";
+console.log(contexte);
